b7fd221a2d3accdc2136f2e9b1b4a10ba7992a32
Javier Gomez <javiergogar12@gmail.com>
2022-08-22 11:51:38 +0200

    Initial commit

---
+++ src/main/java/net/Javier/modprueba/ModPruebaClient.java
@@ -0,0 +1,10 @@
+package net.Javier.modprueba;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModPruebaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
