4fda81386134086777441854d5368475e1aa1048
Javier Gomez <javiergogar12@gmail.com>
2022-08-22 11:57:45 +0200

    añadido comentario prueba para el git

--- src/main/java/net/Javier/modprueba/ModPrueba.java
+++ src/main/java/net/Javier/modprueba/ModPrueba.java
@@ -4,6 +4,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//Comentario prueba para el git
+//Segundo comentario prueba para el git
 public class ModPrueba implements ModInitializer {
 	public static final String MOD_ID = "modprueba";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
